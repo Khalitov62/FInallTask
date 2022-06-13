c409aedd23c43100e142fa2ebb0c08aa7fc7af21
Khalitov62 <tu7m5rk73code.edu.az>
2022-06-13 07:29:52 +0400

    added

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+// let rightBtn = document.querySelector(".carousel-control-prev-icon")
+// let leftBtn = document.querySelector(".carousel-control-next-icon")
+// let images = document.querySelector(".images")
+
+
+// rightBtn.addEventListener("click", function(){
+//     let active = document.querySelector(".active");
+//     active.classList.remove("active");
+//     if (active.nextElementSibling!=null) {
+//         active.nextElementSibling.classList.add("active")
+//     }else{
+//         images.firstElementChild.classList.add("active")
+//     }
+// })
+// leftBtn.addEventListener("click", function(){
+//     let active = document.querySelector(".active");
+//     active.classList.remove("active");
+//     if (active.previousElementSibling!=null) {
+//         active.previousElementSibling.classList.add("active")
+//     }else{
+//         images.firstElementChild.classList.add("active")
+//     }
+// })
